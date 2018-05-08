16d85d5b55149b60c105904ef75a2d087a3c36c1
psirogiannisdimitris <dim.psirogiannis@gmail.com>
2018-05-08 18:43:00 +0300

    add pashe of 4 (mac)

---
+++ app/src/main/java/gr/redpepper/footballrunningtime/PhaseOf4.java
@@ -0,0 +1,13 @@
+package gr.redpepper.footballrunningtime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhaseOf4 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phase_of4);
+    }
+}
